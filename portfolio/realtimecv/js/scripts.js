d83d71340fa80c63776216a703a2d89022720c27
Harun <mdharunbd88@gmail.com>
2021-01-25 19:50:57 +0600

    New Version

--- portfolio/realtimecv/js/scripts.js
+++ portfolio/realtimecv/js/scripts.js
@@ -1,64 +1,67 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    // Modals
+
+
     // ToolTip Active
     $('[data-toggle="tooltip"]').tooltip();
 
-    $('input').on('change', function(){
+    // Progress circle
+    $('input').on('change', function () {
         $('.progress-circle').attr('data-percentage', $(this).val());
-      })
-      
+    });
 
     // Slider
     var big_image = $("#big");
     var thumbs = $("#thumbs");
-    
+
     var syncedSecondary = true;
 
     big_image
-    .owlCarousel({
-        items: 1,
-        slideSpeed: 2000,
-        nav: false,
-        autoplay: false,
-        dots: false,
-        loop: true,
-        responsiveRefreshRate: 200
-        
-    })
-    .on("changed.owl.carousel", syncPosition);
+        .owlCarousel({
+            items: 1,
+            slideSpeed: 2000,
+            nav: false,
+            autoplay: false,
+            dots: false,
+            loop: true,
+            responsiveRefreshRate: 200
+
+        })
+        .on("changed.owl.carousel", syncPosition);
 
     thumbs
-    .on("initialized.owl.carousel", function() {
-        thumbs
-            .find(".owl-item")
-            .eq(0)
-            .addClass("current");
-    })
-    .owlCarousel({
-        dots: false,
-        margin: 15,
-        nav: false,
-        smartSpeed: 200,
-        slideSpeed: 500,
-        slideBy: 4,
-        autoplay: false,
-        responsiveRefreshRate: 100,
-        responsive: {
-            0: {
-                items: 1,
-                dots: true,
-                margin: 0,
-            },
-            600: {
-                items: 1,
-                dots: true,
-            },
-            
-            1024:{
-                items:3
+        .on("initialized.owl.carousel", function () {
+            thumbs
+                .find(".owl-item")
+                .eq(0)
+                .addClass("current");
+        })
+        .owlCarousel({
+            dots: false,
+            margin: 15,
+            nav: false,
+            smartSpeed: 200,
+            slideSpeed: 500,
+            slideBy: 4,
+            autoplay: false,
+            responsiveRefreshRate: 100,
+            responsive: {
+                0: {
+                    items: 1,
+                    dots: true,
+                    margin: 0,
+                },
+                600: {
+                    items: 1,
+                    dots: true,
+                },
+
+                1024: {
+                    items: 3
+                }
             }
-        }
-    })
-    .on("changed.owl.carousel", syncPosition2);
+        })
+        .on("changed.owl.carousel", syncPosition2);
 
     function syncPosition(el) {
         //if loop is set to false, then you have to uncomment the next line
@@ -105,7 +108,7 @@ $(document).ready(function(){
         }
     }
 
-    thumbs.on("click", ".owl-item", function(e) {
+    thumbs.on("click", ".owl-item", function (e) {
         e.preventDefault();
         var number = $(this).index();
         big_image.data("owl.carousel").to(number, 300, true);
@@ -115,25 +118,39 @@ $(document).ready(function(){
 
     // testimonial 
     $('#testimonial').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:false,
-        dots:true,
-        responsive:{
-            0:{
-                items:1
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:1
+            600: {
+                items: 1
             },
-            1000:{
-                items:1
+            1000: {
+                items: 1
             }
         }
     });
 
-    $(function(){
+    $(function () {
         $('.selectpicker').selectpicker();
     });
+
+
+    // range-slider
+
+    $('.range-slider').on('input', function() {
+        $(this).siblings('.fill').css('width', this.value + '%');
+    });
+
+
+    // Experience btn select 
+
+    $(".experience_modal_cont_icons .btn").click(function(){
+        $(this).toggleClass("active-btn").siblings().removeClass("active-btn");
+    })
 
 })
